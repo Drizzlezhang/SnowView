dbebd1b5638e13b57e1a141cff6309a43639dd58
Drizzle <drizzlezhang@gmail.com>
2016-01-08 17:47:08 +0800

    add gif

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # SnowView
 雪花效果
-
+### Sample
+ ![sample1 gif](/png/sample1.gif)
+ ![sample2 gif](/png/sample2.gif)
 #### 设置属性
     snow_level      //雪的大小
     snow_color      //雪花的颜色
-
+    snow_flake_type //雪花类型（纯色或者自定义图片）
     默认为match_parent.
 
 #### 代码中设置
@@ -12,5 +14,7 @@
     snowView.setSnowLevel(...);   //HUGESNOW,BIGSNOW,MIDDLESNOW,SMALLSNOW
 
     snowView.setSnowColor(...);
+    //如果想让雪花显示为图片的话，暂时只支持代码中添加bitmap
+    snowView.setSnowFlakeBitmap(snowFlakeBitmap);
 
 > 其他属性及截图待续。
--- app/src/main/java/com/drizzle/snowview/SnowView.java
+++ app/src/main/java/com/drizzle/snowview/SnowView.java
@@ -2,6 +2,8 @@ package com.drizzle.snowview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -17,8 +19,14 @@ public class SnowView extends View {
 	public int BIGSNOW = 2;
 	public int MIDDLESNOW = 3;
 	public int SMALLSNOW = 4;
+
+	public int SNOWFLAKE_COLOR = 1;
+	public int SNOWFLAKE_BITMAP = 2;
+
 	private int snowColor;
 	private int snowLevel;
+	private int snowflakeType;
+	private Bitmap snowFlakeBitmap;
 
 	public SnowView(Context context) {
 		this(context, null);
@@ -33,12 +41,19 @@ public class SnowView extends View {
 		TypedArray array = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SnowView, defStyleAttr, 0);
 		snowLevel = array.getInt(R.styleable.SnowView_snow_level, BIGSNOW);
 		snowColor = array.getColor(R.styleable.SnowView_snow_color, Color.WHITE);
+		snowflakeType = array.getInt(R.styleable.SnowView_snow_flake_type, SNOWFLAKE_COLOR);
+		snowFlakeBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.snowflake);
 		array.recycle();
 	}
 
 	protected void resize(int width, int height) {
 		Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-		paint.setColor(snowColor);
+		if (snowflakeType == SNOWFLAKE_COLOR) {
+			paint.setColor(snowColor);
+		} else {
+			paint.setFilterBitmap(true);
+			paint.setDither(true);
+		}
 		paint.setStyle(Paint.Style.FILL);
 		snowflakes = new SnowFlake[getSnowNumber(snowLevel)];
 		for (int i = 0; i < getSnowNumber(snowLevel); i++) {
@@ -63,14 +78,16 @@ public class SnowView extends View {
 
 	@Override protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		for (SnowFlake snowFlake : snowflakes) {
-			snowFlake.draw(canvas);
+		if (snowflakeType == SNOWFLAKE_COLOR){
+			for (SnowFlake snowFlake : snowflakes) {
+				snowFlake.draw(canvas);
+			}
+		}else{
+			for (SnowFlake snowFlake : snowflakes) {
+				snowFlake.drawimg(canvas, snowFlakeBitmap);
+			}
 		}
 		getHandler().postDelayed(runnable, DELAY);
-	}
-
-	public int getSnowLevel() {
-		return snowLevel;
 	}
 
 	public void setSnowLevel(int snowLevel) {
@@ -78,12 +95,18 @@ public class SnowView extends View {
 		invalidate();
 	}
 
-	public int getSnowColor() {
-		return snowColor;
-	}
-
 	public void setSnowColor(int snowColor) {
 		this.snowColor = snowColor;
+		invalidate();
+	}
+
+	public void setSnowflakeType(int snowflakeType) {
+		this.snowflakeType = snowflakeType;
+		invalidate();
+	}
+
+	public void setSnowFlakeBitmap(Bitmap snowFlakeBitmap) {
+		this.snowFlakeBitmap = snowFlakeBitmap;
 		invalidate();
 	}
 
